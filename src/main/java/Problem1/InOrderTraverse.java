4b86e90e157d68023b270bd22b7089bc91fd89de
Jacrys3 <ryse0528@gmail.com>
2020-11-13 17:04:50 -0800

    first try on InOrderTraverse

--- src/main/java/Problem1/InOrderTraverse.java
+++ src/main/java/Problem1/InOrderTraverse.java
@@ -2,11 +2,23 @@ package Problem1;
 
 import java.util.ArrayList;
 import java.util.List;
-//
+import java.util.Stack;
+
 public class InOrderTraverse {
     public static List<Integer> inorderTraversalIterative(TreeNode<Integer> root) {
         // homework
         List<Integer> result = new ArrayList<>();
+        Stack<TreeNode<Integer>> tmp = new Stack<>();
+        TreeNode<Integer> temp = root;
+        while(temp != null && !tmp.isEmpty()) {
+            tmp.push(temp);
+            temp = temp.left;
+            if (temp == null) {
+                TreeNode<Integer> pop = tmp.pop();
+                result.add(pop.val);
+                temp = pop.right;
+            }
+        }
         return result;  // place holder
     }
 }
